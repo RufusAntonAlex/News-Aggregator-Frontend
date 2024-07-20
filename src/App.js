b7bf9ee3b7f41ac7ca9e2640394de691bf2d9b1f
Ajjay Adhithya V <156827226+ajjay0604@users.noreply.github.com>
2024-07-20 09:57:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,28 +4,31 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './Components/LoginForm';
 import SignupForm from './Components/SignupForm';
 import Dashboard from './Components/Dashboard';
-import ProtectedRoute from './Components/ProtectedRoute'; // Import the ProtectedRoute component
+import ProtectedRoute from './Components/ProtectedRoute';
+import { UserProvider } from './context/UserContext'; // Import UserProvider
 
-import './App.css'; // Import CSS for global styling
+import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <div className="app">
-        <Routes>
-          <Route exact path="/" element={<LoginForm />} />
-          <Route path="/signup" element={<SignupForm />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
+    <UserProvider>
+      <Router>
+        <div className="app">
+          <Routes>
+            <Route exact path="/" element={<LoginForm />} />
+            <Route path="/signup" element={<SignupForm />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </UserProvider>
   );
 };
 
