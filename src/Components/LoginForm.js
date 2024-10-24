8fb64bbe2f7707080645af9c92b4c4365104c451
Ajjay Adhithya V <156827226+ajjay0604@users.noreply.github.com>
2024-10-24 18:21:32 +0530

    Update LoginForm.js

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -67,7 +67,7 @@ const LoginForm = () => {
         Don't have an account? <Link to="/signup">Signup here</Link>
       </p>
       <p className="info">Please do not refresh the page after logging in</p>
-
+        Note: The language-changing functionality is currently not working but will be fixed soon.
       <p className="language-info">
    
   </p>
